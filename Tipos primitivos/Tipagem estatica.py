005f85a8f2f59257107ffce8614aadef0bbdee93
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-08-16 18:14:15 +0000

    Comandos iniciais python

---
+++ Tipos primitivos/Tipagem estatica.py
@@ -0,0 +1,19 @@
+#TIpagem estatica
+import os 
+
+os.system("cls|| clear") # Limpa o terminal.
+
+
+
+nome: str #cadeia
+idade: int #inteiro
+altura: float #real
+
+
+nome = "Arthur"
+idade = 20
+altura = 1.65
+
+print(f"Nome: {nome}") 
+print(f"Idade: {idade}")
+print(f"Altura: {altura}")
