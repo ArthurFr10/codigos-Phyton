d676d7d6fa7fffcf6e68d1f6a02b72ef803575a8
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-09-01 16:27:42 +0000

    nota com while true e match case

--- Exercicio 23.py
+++ Exercicio 23.py
@@ -8,8 +8,37 @@ O programa deve mostrar a média aritmética para o usuário
 """
 import os
 os.system("cls || clear")
-
+contador = 1
+nota2 = 0
+nota = 0
+nota_final = 0
 nota = float(input("Digite uma nota: "))
 
+while True:
+    print("""
+        -------MENU-------    
+    S - Inserir mais uma nota
+    P - Ver quantas notas foram inseridas
+    N - Calcular a média aritmética das notas informadas.
+    """)
+    opcao = str(input("Digite a letra correspondente: ")).upper()
+    
+    match(opcao):
+        case "S":
+            nota2 = float(input("Insira mais uma nota: "))
+            contador += 1
+            nota_final += nota2
+        case "P":
+            print(f"Quantidade de notas inseridas: {contador}")
+        case "N":
+            
+            media = (nota_final + nota) / contador
+            float(input(f"Média aritmética: {media}"))
+            break
 
+
+            
+        
+
+        
 
