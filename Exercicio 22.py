94fc347adae434a0958ac0255be756c279ca4d90
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-09-01 17:30:16 +0000

    nota com while true e match case

--- Exercicio 22.py
+++ Exercicio 22.py
@@ -1,5 +1,6 @@
 """
-Escreva um algoritmo que pergunte ao usuário se deseja inserir mais uma nota, se a resposta do usuário for "N", o programa fará a média aritmética das notas informadas
+Escreva um algoritmo que pergunte ao usuário se deseja inserir mais uma nota, se a resposta do usuário
+ for "N", o programa fará a média aritmética das notas informadas
 e mostrará a média aritmética para o usuário.
 
 obs: Use um contador dentro do laço de repetição para contar a quantidade de iterações (loops)
@@ -7,9 +8,32 @@ obs: Use um contador dentro do laço de repetição para contar a quantidade de 
 import os
 os.system("cls || clear")
 
+contador = 1
+nota2 = 0
+nota = 0
+nota_final = 0
+nota = float(input("Digite uma nota: "))
 
-opcao = input("Digite uma nota: ")
-contador += 1
+while True:
+    print(f"Quantidade de loops: {contador}")
+    opcao = str(input("Deseja inserir mais uma nota? ")).upper()
+    
+    match(opcao):
+        case "S":
+            nota2 = float(input("Insira mais uma nota: "))
+            
+            contador += 1
+            nota_final += nota2
+
+        case "N":
+            media = (nota_final + nota) / contador
+            float(input(f"Média aritmética: {media}"))
+            break
+
+        case _:
+            print("Tente novamente")
+
+
 
 
        
