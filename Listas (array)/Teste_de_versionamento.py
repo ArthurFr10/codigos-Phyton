b53c825f0917452f5691b0e8da607f55fca389b4
ArthurFr10 <arthurfragabbmp@gmail.com>
2024-09-23 17:29:16 +0000

    salvar

---
+++ Listas (array)/Teste_de_versionamento.py
@@ -0,0 +1,19 @@
+import os
+os.system("cls || clear")
+
+print("ola, mundo")
+
+
+
+#Preparar arquivos param salvar(commit)
+#git status
+#Adicionar arquibvosa pra salvar commit
+#git add .
+
+#Realizando salvamento (commit) git commit - m "mensagem"
+
+#atualizar seu repositorio local em relação ao repositório novo 
+#gitpull
+
+#Envia seu codigo para o git hub
+#git push
