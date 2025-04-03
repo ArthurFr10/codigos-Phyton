4f43787f8e73591d4902b84c2c65b3244d20b8fe
ArthurFr10 <arthurfragabbmp@gmail.com>
2025-04-03 14:26:16 -0300

    Delete ordenacao/ordenacao-main directory

--- ordenacao/ordenacao-main/exercicio.py
+++
@@ -1,9 +0,0 @@
-"""
-Dada uma lista de palavras, escreva programa que solicite ao usuário uma lista de frutas e mostre: 
-- A lista original
-- A lista invertida
-- A lista na ordem inversa
-"""
-import os
-os.system("cls || clear")
-
